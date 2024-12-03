cc67e13f1144063dfe5d6f7dd2669bbd9a546efb
shahiadarsh <shahiadarsh7675@gmail.com>
2024-12-03 19:06:46 +0530

    Commit Changes

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+// Get the baseURL from environment variables
+const baseURL = process.env.REACT_APP_API_BASE_URL;
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Replace with your actual backend base URL
-  withCredentials: true, // If you need to send cookies with requests
+  baseURL: baseURL,  // Use the environment variable for the base URL
+  withCredentials: true, // This is important to send cookies with requests
 });
 
 export default api;
