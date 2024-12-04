afdcccf9935298729c10f9e0e2149072c2111054
shahiadarsh <141950380+shahiadarsh@users.noreply.github.com>
2024-12-04 22:03:01 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,16 +26,16 @@ const Footer = () => {
         ? 'bg-gray-800 text-gray-200'
         : 'bg-blue-600 text-white'
     } p-8 transition-colors duration-300`}>
-      <div className="container mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="container mx-auto ">
+        <div className="flex justify-between gap-8 items-center">
           <div>
-            <h3 className="text-xl font-bold mb-4">Quick Read</h3>
-            <ul className="space-y-2">
+            <h3 className="text-xl font-bold mb-2">Quick Read</h3>
+            <ul className="flex flex-row gap-8 justify-center">
               {blocks.map(block => (
                 <li key={block}>
                   <button
                     onClick={() => handleNavigation(`/${block}`)}
-                    className={`hover:text-blue-300 transition duration-200`}
+                    className={`hover:text-blue-300 transition duration-200 `}
                   >
                     {block}
                   </button>
@@ -44,24 +44,26 @@ const Footer = () => {
             </ul>
           </div>
           <div>
-            <h3 className="text-xl font-bold mb-4">Contact Us</h3>
+            <h3 className="text-xl font-bold mb-1">Contact Us</h3>
             <a
-              href="https://wa.me/919569729522"
+              href="https://wa.me/916393251773"
               target="_blank"
               rel="noopener noreferrer"
-              className={`${
-                darkMode
-                  ? 'bg-green-700 hover:bg-green-600'
-                  : 'bg-green-500 hover:bg-green-600'
-              } text-white px-4 py-2 rounded inline-block transition duration-200`}
+              className={
+              //   `${
+              //   darkMode
+              //     ? 'bg-green-700 hover:bg-green-600'
+              //     : 'bg-green-500 hover:bg-green-600'
+              // }
+               `text-white px-2 py-1 rounded inline-block transition duration-200`}
             >
               WhatsApp: 6393251773
             </a>
-          </div>
+          </div>  
         </div>
         <div className="mt-8 text-center">
           <p className="opacity-75">
-            &copy; {new Date().getFullYear()} Siddharthnagar Times 24. All rights reserved.
+            &copy; {new Date().getFullYear()} Gorakhpur Times 24. All rights reserved.
           </p>
         </div>
       </div>
